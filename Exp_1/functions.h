f63b9966cf894979074f9451d280ddc1bbbab312
TomasIbaceta <33475042+TomasIbaceta@users.noreply.github.com>
2019-10-08 18:14:46 -0300

    functions.h

---
+++ Exp_1/functions.h
@@ -0,0 +1,17 @@
+/*
+ * functions.h
+ *
+ *  Created on: 8 oct. 2019
+ *      Author: Tomas
+ */
+
+#ifndef FUNCTIONS_H_
+#define FUNCTIONS_H_
+
+unsigned int multiply_pow_2_v2 ( unsigned int , unsigned int );
+unsigned int multiply_pow_2_v1 ( unsigned int , unsigned int );
+
+unsigned int multiply_pow_2_v1_ref ( unsigned int*, unsigned int*);
+unsigned int multiply_pow_2_v1_ref ( unsigned int*, unsigned int*);
+
+#endif /* FUNCTIONS_H_ */
